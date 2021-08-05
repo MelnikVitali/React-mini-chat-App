e71b4c97c94b1aeb7d34cbe9ff5c47cda4c5e518
MelnikVitali <mvitalim@gmail.com>
2021-08-06 00:11:58 +0300

    hot fix styles chat component

--- src/components/CreateChannelModal/styles.js
+++ src/components/CreateChannelModal/styles.js
@@ -32,10 +32,6 @@ const useStyles = makeStyles((theme) => ({
         margin: theme.spacing(2, 0, 2),
         paddingLeft:pxToRem(30),
         paddingRight:pxToRem(30),
-        // borderRadius:pxToRem(18)
-        // borderRadius: pxToRem(30),
-        // textTransform: 'capitalize',
-        // lineHeight: 1.2
     }
 }));
 

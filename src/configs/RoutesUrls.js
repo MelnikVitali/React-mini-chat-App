fe9d06f349518a64f38b16fa0e4f521cc1b62f56
MelnikVitali <mvitalim@gmail.com>
2021-08-06 04:39:49 +0300

    hot fix styles chat component

--- src/configs/RoutesUrls.js
+++
@@ -1,4 +0,0 @@
-export const LOGIN_ROUTE = '/';
-export const MAIN_ROUTE = '/main';
-export const CREATE_CHANNEL_ROUTE = '/create-channel';
-export const CHAT_ROUTE = '/chat';
--- src/utils/dummyMessages.js
+++
@@ -1,7 +0,0 @@
-import { v4 as uuidv4 } from 'uuid';
-
-export const dummyMessage = {
-    currentUserId: uuidv4(),
-    nickName: 'Dummy user',
-    text: 'What do you want?',
-};
